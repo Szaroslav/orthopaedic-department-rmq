0371e5d185483f68753af16d3a5cfa9875a31641
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-06-02 16:43:36 +0200

    Extend configuration (#3)

--- src/main/java/pl/edu/agh/orthoment/Configuration.java
+++ src/main/java/pl/edu/agh/orthoment/Configuration.java
@@ -2,4 +2,10 @@ package pl.edu.agh.orthoment;
 
 public class Configuration {
     public static final String EXAMINATION_EXCHANGE = "Examination";
+    public static final String ADMINISTRATION_EXCHANGE = "Administration";
+    public static final String ADMINISTRATION_KEY = "administration";
+    public static final String DOCTOR_0_KEY = "doctor_0";
+    public static final String DOCTOR_1_KEY = "doctor_1";
+    public static final String TECHNICIAN_0_KEY = "technician_0";
+    public static final String TECHNICIAN_1_KEY = "technician_1";
 }
